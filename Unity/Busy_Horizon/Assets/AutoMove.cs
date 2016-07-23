6ec765e88526ecb5e59a0418b032d1514c0774d9
Yuli Levtov <yulilevtov@gmail.com>
2016-07-23 16:30:25 +0100

    Added Busy_Horizon Unity project

---
+++ Unity/Busy_Horizon/Assets/AutoMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMove : MonoBehaviour {
+
+	float speedMin = 0.01f;
+	float speedMax = 0.05f;
+	float radiusMin = 100f;
+	float radiusMax = 362f;
+	float offset = 0f;
+
+	float randSpeed;
+	float randRadius;
+	float randOffset;
+	Quaternion rotationAngle;
+
+	// Use this for initialization
+	void Start () {
+		randSpeed = Random.Range (speedMin, speedMax);
+		randRadius = Random.Range (radiusMin, radiusMax);
+		randOffset = Random.Range (0f, 360f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float posX = Mathf.Sin (Time.time*randSpeed+randOffset) * randRadius;
+		float posZ = Mathf.Cos (Time.time*randSpeed+randOffset) * randRadius;
+		transform.position = new Vector3 (posX, 0, posZ);
+		rotationAngle.eulerAngles = new Vector3(0, Mathf.Sin (Time.time * randSpeed+randOffset)*Mathf.PI, 0);
+		transform.rotation = rotationAngle;
+
+	}
+}
---
+++ Unity/Busy_Horizon/Assets/Scripts/FPS_Mousescript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS_Mousescript : MonoBehaviour {
+
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	float rotationY = 0F;
+	float rotationX = 0F;
+	float multiplier = 1F;
+
+	void Update ()
+	{
+	//	if (axes == RotationAxes.MouseXAndY)
+	//	{
+	//		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+	//
+	//		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+	//		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+	//
+	//		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+	//	}
+	//	else if (axes == RotationAxes.MouseX)
+	//	{
+	//		transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+	//	}
+	//	else
+	//	{
+	//		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+	//		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+	//
+	//		transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		//	}
+
+		multiplier = 1f;
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			multiplier = 3f;
+		}
+
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			rotationX -= 0.4f * multiplier;
+		}
+		if (Input.GetKey(KeyCode.D)) 
+		{
+			rotationX += 0.4f * multiplier;
+		}
+		if (Input.GetKey(KeyCode.W)) 
+		{
+			rotationY -= 0.4f * multiplier;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+		}
+		if (Input.GetKey(KeyCode.S)) 
+		{
+			rotationY += 0.4f * multiplier;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+		}
+		if (Input.GetKey(KeyCode.E)) 
+		{
+			Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView - 1f, 22, 60);
+		}
+		if (Input.GetKey(KeyCode.Q)) 
+		{
+			Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView + 1f, 22, 60);
+		}
+		transform.localEulerAngles = new Vector3(rotationY, rotationX, 0);
+	}
+
+	void Start ()
+	{
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
+			GetComponent<Rigidbody>().freezeRotation = true;
+	}
+
+}
